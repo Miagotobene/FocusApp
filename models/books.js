84261bd5a45e1e5150f2273f87b2b6cabb29c5c9
Miagotobene <miagotobene@gmail.com>
2024-06-08 12:09:29 -0400

    feat: added models folder

---
+++ models/books.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+// Create the book schema here
+const BookSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    publisher: {
+        type: String,
+        required: true
+    },
+    genre: String,
+    cover: String,
+    createdAt: {
+        type: String,
+        default: Date.now()
+    }
+
+});
+
+
+
+
+
+
+module.exports = mongoose.model('Book', BookSchema)
